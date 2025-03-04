0bf178d7719f1ebbfcd2a3db0cb59a4d2f72fa83
Sn0wbelle <S01303821@acad.tri-c.edu>
2025-03-04 14:01:31 -0500

    Mini Exam 2

---
+++ Mini Exam 2/Mini Exam 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+class MiniExam2
+{
+    static double CylinderVolume(double radius, double height)
+    {
+        double cylVolume = 3.14 * radius * radius * height;
+        return cylVolume;
+    }
+    static void Main()
+    {
+        double radius, height, volume;
+        
+        Console.Write("Enter the radius of the cylinder: ");
+        radius = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the height of the cylinder: ");
+        height = Convert.ToDouble(Console.ReadLine());
+
+        volume = CylinderVolume(radius, height);
+
+        Console.WriteLine("The volume of the cylinder is: " + volume);      
+    }
+}
